40283e3445e8f6ab6360e46c52d455cf5befd712
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-09-19 10:16:03 -0500

    groceries can toggle

--- client/components/GroceryList.js
+++ client/components/GroceryList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import GroceryItem from './GroceryItem';
 import { connect } from 'react-redux';
+import { toggleGrocery } from '../store';
+
 const GroceryList = props => (
   <ul>
     {props.groceries.map(grocery => (
-      <GroceryItem key={grocery.id} {...grocery} />
+      <GroceryItem
+        key={grocery.id}
+        {...grocery}
+        onClick={() => props.toggleGrocery(grocery.id)}
+      />
     ))}
   </ul>
 );
@@ -13,4 +19,14 @@ const mapState = state => ({
   groceries: state.groceries,
 });
 
-export default connect(mapState)(GroceryList);
+const mapDispatch = dispatch => {
+  return {
+    toggleGrocery: groceryId => {
+      dispatch(toggleGrocery(groceryId));
+    },
+  };
+};
+export default connect(
+  mapState,
+  mapDispatch
+)(GroceryList);
