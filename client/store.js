70672672a2e11d013c24039d576a78a623ef2c98
Bryan Gottschalk <bryanlgottschalk@gmail.com>
2019-09-19 09:52:49 -0500

    toggle grocery

--- client/store.js
+++ client/store.js
@@ -2,12 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import loggerMiddleware from 'redux-logger';
 
 const ADD_GROCERY = 'ADD_GROCERY';
+const TOGGLE_GROCERY = 'TOGGLE_GROCERY';
 
 let nextId = 0;
 export const addGrocery = text => ({
   type: ADD_GROCERY,
   id: nextId++,
   text,
+});
+
+export const toggleGrocery = groceryId => ({
+  type: TOGGLE_GROCERY,
+  id: groceryId,
 });
 
 const initialState = { groceries: [] };
@@ -20,6 +26,15 @@ const reducer = (state = initialState, action) => {
         bought: false,
       };
       return { ...state, groceries: [...state.groceries, newGrocery] };
+    case TOGGLE_GROCERY:
+      const groceries = state.groceries.map(grocery => {
+        if (grocery.id === action.id) {
+          return { ...grocery, bought: !grocery.bought };
+        } else {
+          return grocery;
+        }
+      });
+      return { ...state, groceries };
     default:
       return state;
   }
